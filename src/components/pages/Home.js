ebdea765167536228534829878e2503c26e6ac70
clairebogdan <bogdanclaire@gmail.com>
2020-10-12 16:15:26 -0400

    added photos and updated Order and Home pages

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../../App.css'
+import '../styles/Home.css'
 import OrderForm from './OrderForm';
 
 export default function Home() {
     return (
         <>
-        <h1>this is the homepage</h1>
-        
+        <div className='palms'>
+            <div className='home-text-outer'>
+                <div className='home-text-inner'>
+                    <h1 className='welcome-text'>Bogdan Reeds</h1>
+                </div>
+            </div>
+        </div>
+
         <OrderForm/>
         </>
     );
--- src/components/pages/Order.js
+++ src/components/pages/Order.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import '../../App.css'
+import '../styles/Order.css';
 import OrderForm from './OrderForm';
+import label from '../assets/bogdanreeds-label.png'
 
 export default function Order() {
     return (
+        <>
+        <div className='label'>
+            <img className='img' src={label} alt="label for bogdan reeds"/>
+        </div>
+
+        <div className="order-desc">
+            <p>The order inquiry form below will email me all the necessary information that I will 
+                need for your order. This form does NOT make your order official. Rather, it begins 
+                our conversation and speeds up the process. 
+                <br/><br/>
+                Reeds are $25 each. Standard shipping is $5, and priority shipping is $8.
+            </p>
+        </div>
+
         <OrderForm/>
+        </>
     );
 }
