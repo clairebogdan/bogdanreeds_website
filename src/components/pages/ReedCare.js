81e2429d70f582475e3b69ca332f724bc07ce33b
clairebogdan <bogdanclaire@gmail.com>
2020-10-12 21:40:33 -0400

    updated Reed Care page

--- src/components/pages/ReedCare.js
+++ src/components/pages/ReedCare.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import '../../App.css'
+import '../styles/ReedCare.css'
 
 export default function ReedCare() {
     return (
         <>
-        <h1>this is the reed care</h1>
+        <div className='reed-care-box'>
+            <h1 className="topic">General Tips</h1>
+            <ul>
+                <li>hi</li>
+            </ul>
+        </div>
+
+        <div className='reed-care-box'>
+            <h1 className="topic">Help</h1>
+            <div className="arrow-grid">
+                <div className='grid-item'>
+                    <div className="if">If...</div>
+                    <div className='arrow'><i class="fas fa-long-arrow-alt-right"></i></div>
+                    <div className='then'>Then...</div>
+                </div>
+            </div>
+        </div>
         </>
     );
 }
